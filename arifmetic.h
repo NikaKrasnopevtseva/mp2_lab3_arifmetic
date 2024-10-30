40e7f9decda92df657cb26974f908c697f20e3c5
NikaKrasnopevtseva <144881960+NikaKrasnopevtseva@users.noreply.github.com>
2024-10-30 12:10:11 +0300

    Update arifmetic.h

--- arifmetic.h
+++ arifmetic.h
@@ -4,29 +4,76 @@
 
 #include <iostream>
 #include <string>
+#include <cctype>
 using namespace std;
-//Type: 1 - number, 2 - variable, 3 - operation, 4 - parenthesis
-class Lexema {
-	int length;
-	int Type;
-	double number;
-	char name;
-public:
-	Lexema() : length(0), Type(0), number(0) {}
-	Lexema(int l,int t,double num,char name) {
+bool containsChar(char arr[], char ch, int size) {
+	for (int i = 0; i < size; ++i) {
+		if (arr[i] == ch) {
+			return 1; // Символ найден
+		}
+	}
+	return 0; // Символ не найден
+}
+//Type: 1 - operation, 2 - variable, 3 - number , 4 - function
+char signs[] = {'(',')','+','-','*','/'};
 
+class Lexema {
+	string str;
+	int type;
+	double number; //храним числа и храним занчение переменной
+public:
+	Lexema(string s, int t) {
+		str = s;
+		type = t;
+		if (type == 2)
+			number = stod(s, nullptr);
 	}
-	void PrintName() {
-		cout << name << endl;
+	void PrintStr() {
+		cout << str << endl;
 	}
-	char GetName() { return name; }
+	string GetName() { return str; }
+	void SetName(string name) { str = name; }
 	double GetNumber() { return number; }
 	void SetNumber(double a) { number = a; }
-	int GetLength() { return length; }
-	void SetLength(int i) { length = i; }
-	int GetType() { return Type; }
-	void SetType(int i) { Type = i; }
-	void SetChar(char a) { name = a; }
+	int GetType(char c) { 
+		if (isalpha(c)) return 1;
+		else if ((isdigit(c)) || (c == ',')) return 2;
+		else if (containsChar(signs,c,6)) return 3;
+		else return -1;
+	}
+	void SetType(int i) { type = i; }
+};
+
+class Arifmetic {
+	string inputstr;
+	vector <Lexema> input;
+	vector <Lexema> postfix;
+	string postfixstr;
+public:
+	Arifmetic(string str) //конструктор
+	{
+
+	}
+	void GoToLexems(string str) //делим строку на лексемы (получаем vector <Lexema> input)
+	{
+
+	}
+	void GoToPostfix(string str) //делаем постфиксную запись (получаем vector <Lexema> postfix)
+	{
+
+	}
+	void GoToPostfixStr(string str) //делаем постфиксную запись (получаем string postfixstr)
+	{
+
+	}
+	double Calculate() //по постфиксной записи вычисляем выражение
+	{
+
+	}
+	void GetValues() //
+	{
+
+	}
 };
 
 
